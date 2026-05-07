6de1721279f78ca9110152be7d5a1fc93ddbd0db
Laurent <laurent.pezard@univ-amu.fr>
2026-05-07 08:27:07 +0200

    Stochastic now deals in a clear manner with unobserved symbols

--- src/scyseq/stochastic.py
+++ src/scyseq/stochastic.py
@@ -11,9 +11,11 @@ from . import operations as O
 
 # warnings.simplefilter('always', RuntimeWarning)
 
-def conditional_matrix(dependent, conditioning, smooth=0):
+def conditional_matrix(dependent, conditioning, smooth=None):
     """
     Returns the conditional matrix ie P(y=j | x=i).
+
+    This is estimated using the maximum likelihood estimator.
 
     P(x) = 0 is dealt with add-k smoothing:
 
@@ -21,16 +23,17 @@ def conditional_matrix(dependent, conditioning, smooth=0):
 
     with :math:`|A_y|` the alphabet length of the dependent sequence
 
-    This is estimated using the maximum likelihood estimator.
-
-    :param seq1: a symbolic Sequence object
-    :param seq2: a symbolic Sequence object
+    :param dependent: a symbolic Sequence object
+    :param conditioning: a symbolic Sequence object
+    :param smooth: smoothing with add-k (see below)
 
     :returns: A numpy.array of floats
 
-    ..todo::
+    If smooth is None: no smoothing is applied. Can lead to non-stochastic
+    matrices with NaN due to P(x) = 0
 
-        check the doc and implementation of conditional_matrix
+    If smooth == 0 and P(x) == 0 raises an exception (cannot compute the
+    stochastic matrix)
 
     NB: lines should sum to one (one should go somewhere) see
     markov_sequence in generate.py
@@ -51,12 +54,6 @@ def conditional_matrix(dependent, conditioning, smooth=0):
     """
     dseq, cseq = dependent, conditioning
     assert len(dseq) == len(cseq), 'Sequence should have the same length.'
-    if not isinstance(smooth, (int, float, np.integer)):
-        raise ValueError(f"Smooth should be float or integer not {type(smooth)}")
-
-# FIXME: Give a better exception
-    if any(cseq.count() == 0) and smooth == 0:
-        raise ValueError('Cannot compute conditional probabilities with null marginals and no smoothing')
 
     d_alen = dseq.k
     c_alen = cseq.k
@@ -80,26 +77,25 @@ def conditional_matrix(dependent, conditioning, smooth=0):
 #    #  ...] so we need to transpose
     p_marg =  np.reshape(np.tile(fq_marg, d_alen), (d_alen, -1)).T
 
-# Add-k smoothing
-    p_cond = (p_join + smooth) / (p_marg + smooth * d_alen)
-    testing.assert_allclose(np.sum(p_cond, 1), 1)
-    return p_cond
+    if smooth is None:
 
-#    ind0 = np.where(p_marg == 0.)
-#    p_marg[ind0] = 1 # avoid divide by zero
-#    p_cond = p_join / p_marg
-#    # print(p_cond, fq_marg)
-#    # set nulls -> nulls transitions / conditions = 1
-#    nulls = np.where(fq_marg==0.)
-#    # p_cond[nulls[0], nulls[0]] = 1
-#    #print(nulls)
-#    p_cond[nulls] = 1
-#    print(p_cond)
-#    testing.assert_allclose(np.sum(p_cond, 1), 1)
-#
-#    # FIXME: add a warning for P(x) = 0 and / or P(x,y)=0 P(y)=0
-#    
-#    return np.array(p_cond)
+        with np.errstate(divide='ignore', invalid='ignore'):
+            p_cond = p_join / p_marg
+            p_cond[np.isnan(p_cond)] = np.nan  # Explicit
+        return p_cond
+
+    elif smooth == 0 and any(cseq.count() == 0):
+            raise ValueError('Cannot compute conditional probabilities with null marginals and zero smoothing')
+
+    else:
+
+        if smooth < 0:
+            raise ValueError('smooth should be positive')
+
+# Add-k smoothing
+        p_cond = (p_join + smooth) / (p_marg + smooth * d_alen)
+        testing.assert_allclose(np.sum(p_cond, 1), 1)
+        return p_cond
 
 def transition_matrix(seq, time=1, smooth=0):
     """
@@ -116,12 +112,12 @@ def transition_matrix(seq, time=1, smooth=0):
     >>> np.random.seed(9)
     >>> a = np.random.choice([0,1],1000,replace=True, p=[0.7,0.3])
     >>> A = S.Alphabet(['a','b'])
-    >>> seq = S.Sequence(a,A)
+    >>> seq = S.Sequence(a, A)
     >>> transition_matrix(seq)
     array([[0.70224719, 0.29775281],
            [0.73519164, 0.26480836]])
     """
-    # Do not change: not tested (or write test...)
+
     return conditional_matrix(seq[time:], seq[:-time], smooth=smooth)
 
 def influence_matrix(seq1, seq2, time=1, smooth=0):
@@ -148,6 +144,6 @@ def influence_matrix(seq1, seq2, time=1, smooth=0):
     array([[0.70887918, 0.29112082],
            [0.71794872, 0.28205128]])
     """
-    # Do not change: not tested (or write test...)
+
     return conditional_matrix(seq1[time:], seq2[:-time], smooth=smooth)
 
