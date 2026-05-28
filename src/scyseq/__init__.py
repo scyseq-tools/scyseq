4472383a85e08c03bddd185a6b5d8fa6e294fcbf
Laurent Pezard <laurent.pezard@univ-amu.fr>
2026-05-28 12:03:26 +0200

    pytests passed, no more fix with hatch fmt, and ruff check all passed (need unsafe-fix with hatch)

--- src/scyseq/__init__.py
+++ src/scyseq/__init__.py
@@ -15,15 +15,33 @@ __authors__ = [
     "Dewmith Weerasena",
 ]
 
-# from scyseq.exceptions import *
+from .exceptions import (
+    AlphabetAccessError,
+    AlphabetError,
+    EmptyAlphabetError,
+    InvalidSymbolError,
+    LengthError,
+    ScyseqError,
+    SequenceError,
+    SequenceParseError,
+    SymbolAccessError,
+    SymbolDefinitionError,
+    SymbolError,
+    SymbolMismatchError,
+)
 from .io import read_codix  #, write_codix
 from .operations import count, frequency, recode, reduce, rename, reverse, roll, shuffle, transform, words
 from .sequence import Alphabet, Sequence, Symbol, binary_alphabet, boolean_alphabet
 
-__all__ = ["Symbol", "Alphabet", "Sequence", 
+__all__ = ["Symbol", "Alphabet", "Sequence",
            "binary_alphabet", "boolean_alphabet",
            "count", "frequency", "recode", "reduce", "rename", "reverse",
-           "roll", "shuffle", "transform", "words", "read_codix"]
+           "roll", "shuffle", "transform", "words",
+           "read_codix",
+           "ScyseqError", "SymbolError", "SymbolDefinitionError",
+           "SymbolAccessError", "AlphabetError", "AlphabetAccessError",
+           "InvalidSymbolError", "EmptyAlphabetError", "SequenceError",
+           "SequenceParseError", "LengthError", "SymbolMismatchError" ]
 
 def __dir__():
     return __all__
